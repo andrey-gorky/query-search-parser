54bbf94902e7de69efbb6a9ac2566b6af519cc8a
Andrey Gorky <gorkaviya@gmail.com>
2021-11-06 21:17:33 +0200

    modified a bit

--- src/parser.js
+++ src/parser.js
@@ -4,54 +4,8 @@ const url = require('url');
 
 const urlValidityCheck = url => {
     if (url.length < 1) return null;
-    if (url.replace('?', '').includes('?') > 0) return console.log('===========URL=============', url);
-    // if (url.includes('?')) throw new Error('URL is not valid');
+    if (url.lastIndexOf('?') > 0) throw new Error('URL is not valid');
 }
-
-// const keySplitter = (key) => {
-//     if (key.includes('.')) {
-//        let keys = key.split('.');
-//        let result;
-//        for (let i = 0; i < keys.length; i++) {
-//            keys[i].keys[i+1];
-//        }
-//     }
-// }
-
-//!!!!!! const keySplitter = (key) => {
-//     let tempObject = {}
-//     if (key.includes('.')) {
-//         tempObject = JSON.parse('{"' + key.replace(/./g, '": {"').replace(/&/g, '","').replace(/=/g,'":"') + '}')
-//     }
-//     return key;
-// }
-// JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
-
-//!!!!!!!!! function index(obj,is, value) {
-//     if (typeof is == 'string'){
-//         return index(obj,is.split('.'), value);
-//     } else if (is.length==1 && value!==undefined) {
-//         return obj[is[0]] = value;
-//     } else if (is.length==0) {
-//         return obj;
-//     }
-//     else {
-//         let result = index(obj[is[0]],is.slice(1), value);
-//         console.log('==========RESUUUUULT=======', result);
-//         return index(obj[is[0]],is.slice(1), value);
-//     }
-// }
-
-//!!1 const recompose = (obj, string, value) => {
-//     var parts = string.split('.');
-//     var newObj = obj[parts[0]];
-//     if (parts[1]) {
-//       parts.splice(0, 1);
-//       var newString = parts.join('.');
-//       return recompose(newObj, newString);
-//     }
-//     return newObj;
-// }
 
 const keySplitter = (keys, value) => {
     var tempObject = {};
@@ -63,7 +17,6 @@ const keySplitter = (keys, value) => {
         });
     return tempObject
 }
-
 
 const clean = (value) => {
     let checkIfTrue = ['true'].includes(value);
@@ -79,7 +32,7 @@ const clean = (value) => {
 const parser = (query) => {
     const queryURL = new URL(query).search;
     if (queryURL.length < 1) return null;
-    if (queryURL.replace('?', '').includes('?') > 0) throw new Error('URL is invalid');
+    if (queryURL.lastIndexOf('?') > 0) throw new Error('URL is invalid');
     const params = new URLSearchParams(queryURL);
 
     let jsonObj = {}
